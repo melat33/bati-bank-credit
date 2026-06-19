20729f58bbe0187dd6d74e31ca93cd6c9e286b3e
Melat Tewachew <melattewachew@gmail.com>
2026-06-19 10:19:28 +0300

    ix: update CORS configuration to fix frontend-backend connection

--- src/api/main.py
+++ src/api/main.py
@@ -31,7 +31,9 @@ app.add_middleware(
     allow_origins=[
     "http://localhost:3000",
     "http://192.168.3.113:3000",
-    "https://bati-bank-credit.vercel.app/",
+    "https://bati-bank-credit.vercel.app",
+    "https://bati-bank-api-production.up.railway.app",
+
 ], # React dev server
     allow_methods    =["*"],
     allow_headers    =["*"],
